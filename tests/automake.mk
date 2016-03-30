41e2fefa29c70d8b8f70719daab74b4d5ce90c8c
William Tu <u9012063@gmail.com>
2016-03-29 19:04:58 -0700

    tmp

--- tests/automake.mk
+++ tests/automake.mk
@@ -213,9 +213,7 @@ valgrind_overlap='Source and destination overlap in'
 valgrind_output_dir=$(abs_top_srcdir)/tests/testsuite.dir
 
 #VALGRIND = valgrind --log-file=$(valgrind_output_dir)valgrind.%p --leak-check=full --errors-for-leak-kinds=definite
-VALGRIND = valgrind --log-file=$(valgrind_output_dir)/valgrind.%p --leak-check=full \
-	--suppressions=$(abs_top_srcdir)/tests/glibc.supp \
-    --suppressions=$(abs_top_srcdir)/tests/openssl.supp 
+VALGRIND = valgrind --log-file=$(valgrind_output_dir)/valgrind.%p --leak-check=full  
 EXTRA_DIST += tests/glibc.supp tests/openssl.supp
 check-valgrind: all tests/atconfig tests/atlocal $(TESTSUITE) \
                 $(valgrind_wrappers) $(check_DATA)
