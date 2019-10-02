44cd233141f6542cb97e824eba943d911af819ac
William Tu <u9012063@gmail.com>
2019-10-02 13:26:57 -0700

    netdev-linux: Detect numa node id.
    The patch detects the numa node id from the name of the netdev,
    by reading the '/sys/class/net/<devname>/device/numa_node'.
    If not available, ex: virtual device, or any error happens,
    return numa id 0.  Currently only the afxdp netdev type uses it,
    other linux netdev types are disabled due to no use case.
    
    Signed-off-by: William Tu <u9012063@gmail.com>
    ---
    v5:
      Feedbacks from Ilya
      - Tested-at: https://travis-ci.org/williamtu/ovs-travis/builds/601938383
    
    v4:
      Feedbacks from Eelco
      - Tested-at: https://travis-ci.org/williamtu/ovs-travis/builds/599308893
    
    v3:
      Feedbacks from Ilya and Eelco
      - update doc, afxdp.rst
      - fix coding style
      - fix limit of numa node max, by using ovs_numa_numa_id_is_valid
      - move the function to netdev-linux
      - cache the result of numa_id
      - add security check for netdev name
      - use fscanf instead of read and convert to int
      - revise some error message content
    
    v2:
      address feedback from Eelco
    	fix memory leak of xaspintf
    	log using INFO instead of WARN

--- lib/netdev-linux.c
+++ lib/netdev-linux.c
@@ -236,6 +236,7 @@ enum {
     VALID_VPORT_STAT_ERROR  = 1 << 5,
     VALID_DRVINFO           = 1 << 6,
     VALID_FEATURES          = 1 << 7,
+    VALID_NUMA_ID           = 1 << 8,
 };
 
 struct linux_lag_slave {
@@ -1389,6 +1390,66 @@ netdev_linux_tap_batch_send(struct netdev *netdev_,
         }
     }
     return 0;
+}
+
+static int
+netdev_linux_get_numa_id__(const struct netdev *netdev_)
+{
+    struct netdev_linux *netdev = netdev_linux_cast(netdev_);
+    char *numa_node_path;
+    const char *name;
+    int node_id;
+    FILE *stream;
+
+    if (netdev->cache_valid & VALID_NUMA_ID) {
+        return netdev->numa_id;
+    }
+
+    netdev->numa_id = 0;
+    netdev->cache_valid |= VALID_NUMA_ID;
+
+    name = netdev_get_name(netdev_);
+    if (strpbrk(name, "/\\")) {
+        VLOG_ERR_RL(&rl, "\"%s\" is not a valid name for a port. "
+                    "A valid name must not include '/' or '\\'."
+                    "Using numa_id 0", name);
+        return 0;
+    }
+
+    numa_node_path = xasprintf("/sys/class/net/%s/device/numa_node", name);
+
+    stream = fopen(numa_node_path, "r");
+    if (!stream) {
+        /* Virtual device does not have this info. */
+        VLOG_INFO_RL(&rl, "%s: Can't open '%s': %s, using numa_id 0",
+                     name, numa_node_path, ovs_strerror(errno));
+        free(numa_node_path);
+        return 0;
+    }
+
+    if (fscanf(stream, "%d", &node_id) != 1
+        || !ovs_numa_numa_id_is_valid(node_id))  {
+        VLOG_WARN_RL(&rl, "%s: Can't detect NUMA node, using numa_id 0", name);
+        node_id = 0;
+    }
+
+    netdev->numa_id = node_id;
+    fclose(stream);
+    free(numa_node_path);
+    return node_id;
+}
+
+static int OVS_UNUSED
+netdev_linux_get_numa_id(const struct netdev *netdev_)
+{
+    struct netdev_linux *netdev = netdev_linux_cast(netdev_);
+    int numa_id;
+
+    ovs_mutex_lock(&netdev->mutex);
+    numa_id = netdev_linux_get_numa_id__(netdev_);
+    ovs_mutex_unlock(&netdev->mutex);
+
+    return numa_id;
 }
 
 /* Sends 'batch' on 'netdev'.  Returns 0 if successful, otherwise a positive
@@ -3298,7 +3359,7 @@ const struct netdev_class netdev_afxdp_class = {
     .set_config = netdev_afxdp_set_config,
     .get_config = netdev_afxdp_get_config,
     .reconfigure = netdev_afxdp_reconfigure,
-    .get_numa_id = netdev_afxdp_get_numa_id,
+    .get_numa_id = netdev_linux_get_numa_id,
     .send = netdev_afxdp_batch_send,
     .rxq_construct = netdev_afxdp_rxq_construct,
     .rxq_destruct = netdev_afxdp_rxq_destruct,
