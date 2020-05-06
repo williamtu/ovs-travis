0e6ea6a03a55cc7ea5aceb4796a12ef9ab560880
Harry van Haaren <harry.van.haaren@intel.com>
2020-05-06 14:06:07 +0100

    dpcls: enable cpu feature detection
    This commit implements a method to retrieve the CPU ISA capabilities.
    These ISA capabilities can be used in OVS to select a function
    implementation that uses the best ISA available on the CPU being used.
    
    Signed-off-by: Harry van Haaren <harry.van.haaren@intel.com>
    Signed-off-by: William Tu <u9012063@gmail.com>

--- lib/dpdk-stub.c
+++ lib/dpdk-stub.c
@@ -79,6 +79,19 @@ print_dpdk_version(void)
 {
 }
 
+int
+dpdk_get_cpu_has_isa(const char *arch OVS_UNUSED,
+                     const char *feature OVS_UNUSED)
+{
+    static struct ovsthread_once once = OVSTHREAD_ONCE_INITIALIZER;
+    if (ovsthread_once_start(&once)) {
+        VLOG_ERR("DPDK not supported in this version of Open vSwitch, "
+                 "cannot use CPU flag based optimizations");
+        ovsthread_once_done(&once);
+    }
+    return 0;
+}
+
 void
 dpdk_status(const struct ovsrec_open_vswitch *cfg)
 {
